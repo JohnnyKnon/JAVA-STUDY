f6a4b120120c828a78ab73900bfcdb002b7ec714
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-06-20 20:57:18 +0900

    Hello world 실행

---
+++ HelloWorld/HelloWorldApp.java
@@ -0,0 +1,9 @@
+public class HelloWorldApp{
+// HelloWorldApp은 파일명과 같아야함
+// 동명의 클래스를 찾기 때문에 같음
+	public static void main(String[] args) {
+// 	main 이라는 약속된 이름의 메소드를 찾
+// 그리고 중괄호 안의 내용을 출력하게 
+		System.out.println("Hello World!!");
+	}
+}
