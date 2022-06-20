9a80c872559c3be2539c72f20cacbf236bb5ae55
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-06-20 23:29:35 +0900

    변수를 이용한 짤막한 코드

---
+++ Data_and_operation/Letter.java
@@ -0,0 +1,14 @@
+
+public class Letter {
+
+	public static void main(String[] args) {
+//		이름 변수
+		String name = "egoing";
+//		편지를 보낼때.
+		System.out.println("Hello," + name + "..."+ name + "... egoing ...bye");
+//		VAT
+		double VAT = 10.0;
+		System.out.println(VAT);
+	}
+
+}
