a14263c389b66c7eafa5eb22d50a45d4d678ffbd
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-06-20 22:21:29 +0900

    String

---
+++ Data_and_operation/StringApp.java
@@ -0,0 +1,21 @@
+
+public class StringApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World"); // String : Character 가 모여있는 것이 String
+		System.out.println('H'); // 특수한 데이터 타입 Character : 한 글자(문자) 표현
+		
+		System.out.println("Hello "
+				
+				+ "World"); // 이클립스가 개입한 것, + 결합연산자라 위와 똑같음
+		
+		// new line : \n 줄바꿈
+		System.out.println("Hello \nWorld");
+		
+		// escape	\"world\" : 역슬래쉬를 통하여 뒤에 따라오는 임무가 있는 문자의 임무를 해방시키는 것.	
+		System.out.println("Hello \"World\""); // Hello "World"
+	}
+
+}
