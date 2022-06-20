498ebe39705f0944a0d5bbf8a86fc62be418077d
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-06-20 23:21:49 +0900

    Variable

---
+++ Data_and_operation/Variable.java
@@ -0,0 +1,19 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+//		Variable : 값이 변할 수 있는 문자를 의미 
+		
+		// Number -> integer : 정수 | 음수, 0, 자연수를 합친것 = 정수 -> int
+		int a = 1; 
+		System.out.println(a);
+		// Real Number -> 정수가 아닌 수 | 소수와 같은 실수들 -> double 
+		double b = 1.1;
+		System.out.println(b);
+		// String -> 문자열
+		String c = "Hello World";
+		System.out.println(c);
+
+	}
+
+}
