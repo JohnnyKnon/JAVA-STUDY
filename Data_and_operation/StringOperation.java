6dd1f196075c98fd418710295fb90bfb4bd50477
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-06-20 23:06:27 +0900

    String Operate 예시

---
+++ Data_and_operation/StringOperation.java
@@ -0,0 +1,14 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// 몇 글자인지 알려줌
+		System.out.println("Hello World".length()); 
+		//	[[Name]] 을 egoing으로 재배치	
+		System.out.println("Hello, [[Name]]...bye.".replace("[[Name]]", "dorumamu"));
+
+	}
+
+}
