737661077b164033b7e83fcb937021b79fe02f87
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-06-20 22:09:46 +0900

    Number와 Math를 이용한 계산

---
+++ Data_and_operation/Number.java
@@ -0,0 +1,21 @@
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Operator ----------------------------
+		System.out.println(6 + 2); // 8
+		System.out.println(6 - 2); // 4
+		System.out.println(6 * 2); // 12
+		System.out.println(6 / 2); // 3
+
+//		Math : 수학과 관련해서 유용하고 자주 사용되는 것을 그루핑한 것
+		System.out.println(Math.PI); // PI 값
+//		내림 
+		System.out.println(Math.floor(Math.PI)); // 3.0
+//		올림
+		System.out.println(Math.ceil(Math.PI)); // 4.0
+	}
+
+}
