548f198ad32b6c3e7e3aba5da236b87124067fdd
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-06-21 22:35:41 +0900

    DimmingLights를 위하여 밝기 조절을 만듦
    그냥 String 값으로만 받으면 에러가 난다.
    그렇기에 String -> int or String-> Double을 해야함,
    
    Double.parseDouble(i);
    를 이용하여 Double 실수형으로 Cast해줌

--- Programming/OkjavaGoHomePopUpInput.java
+++ Programming/OkjavaGoHomePopUpInput.java
@@ -1,4 +1,5 @@
 import org.opentutorials.iot.Security;
+import org.opentutorials.iot.DimmingLights;
 import org.opentutorials.iot.Elevator;
 import org.opentutorials.iot.Lighting;
 import javax.swing.JOptionPane;
@@ -9,6 +10,10 @@ public class OkjavaGoHomePopUpInput {
 		
 		String id = JOptionPane.showInputDialog("Enter your home ID(Address)");
 		String roomID = id;
+		
+		String Brightness = JOptionPane.showInputDialog("Enter Brightness");
+//		Integer.parseInt를 통하여,String 값을, Int로 변경 후 반
+		double brightValue = Double.parseDouble(Brightness);
 	
 		// Elevator call
 		Elevator myElevator = new Elevator(roomID);
@@ -23,6 +28,10 @@ public class OkjavaGoHomePopUpInput {
 		
 		Lighting toiletLamp = new Lighting(roomID + " / Toilet Lamp");
 		toiletLamp.on();
+		
+		DimmingLights moodLamp = new DimmingLights(id +"moodLamp");
+		moodLamp.setBright(brightValue);
+		moodLamp.on();
 
 	}
 
