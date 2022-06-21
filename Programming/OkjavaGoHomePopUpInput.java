883198552ad95f0ff58716d2d3655ec833fda751
Johnny hong <86140440+JohnnyKnon@users.noreply.github.com>
2022-06-21 23:14:08 +0900

    Args 등록후 Arg를 이용하여 실행

--- Programming/OkjavaGoHomePopUpInput.java
+++ Programming/OkjavaGoHomePopUpInput.java
@@ -5,15 +5,18 @@ import org.opentutorials.iot.Lighting;
 import javax.swing.JOptionPane;
 
 public class OkjavaGoHomePopUpInput {
-
+// args는 parameter 즉 매개변수.
 	public static void main(String[] args) {
 		
-		String id = JOptionPane.showInputDialog("Enter your home ID(Address)");
-		String roomID = id;
+//		String id = JOptionPane.showInputDialog("Enter your home ID(Address)");
+//		String roomID = id;
+//		
+//		String Brightness = JOptionPane.showInputDialog("Enter Brightness");
+////		Integer.parseInt를 통하여,String 값을, Int로 변경 후 반
+//		double brightValue = Double.parseDouble(Brightness);
 		
-		String Brightness = JOptionPane.showInputDialog("Enter Brightness");
-//		Integer.parseInt를 통하여,String 값을, Int로 변경 후 반
-		double brightValue = Double.parseDouble(Brightness);
+		String roomID = args[0]; //args[0] index로 부터 시작 하므로 1번째, [0]인자값인, JAVA APT 507 호출
+		String bright = args[1]; // [1]인자값인, 25.1
 	
 		// Elevator call
 		Elevator myElevator = new Elevator(roomID);
@@ -29,8 +32,8 @@ public class OkjavaGoHomePopUpInput {
 		Lighting toiletLamp = new Lighting(roomID + " / Toilet Lamp");
 		toiletLamp.on();
 		
-		DimmingLights moodLamp = new DimmingLights(id +"moodLamp");
-		moodLamp.setBright(brightValue);
+		DimmingLights moodLamp = new DimmingLights(roomID +"moodLamp");
+		moodLamp.setBright(Double.parseDouble(bright));
 		moodLamp.on();
 
 	}
